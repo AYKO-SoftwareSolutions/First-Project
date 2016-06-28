95527dc8b183d7eed8a2ab39d2404fa82367ab82
Yogaraj <yogaraj.s@above-inc.com>
2016-06-28 23:48:59 +0530

    Initial angular setup for login module

---
+++ HTML/scripts/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// declare modules
+angular.module('Login', []);
+ 
+.config(['$routeProvider', function ($routeProvider) {
+
+    $routeProvider
+        .when('/login', {
+            controller: 'LoginController',
+            templateUrl: 'modules/authentication/views/login.html',
+            hideMenus: true
+        })
+ 
+        .when('/', {
+            controller: 'HomeController',
+            templateUrl: 'modules/home/views/home.html'
+        })
+ 
+        .otherwise({ redirectTo: '/login' });
+}])
